45facaade587debfb28b14154695a4f1ef1d08a3
Kevin <kevin.rph.lee@gmail.com>
2022-04-29 23:37:57 -0700

    added client and server.js template

---
+++ server.js
@@ -0,0 +1,51 @@
+// server/index.js
+require('dotenv').config();
+
+const express = require("express");
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+
+// PG database client/connection setup
+const { Pool } = require('pg');
+const dbParams = require('./lib/db.js');
+const db = new Pool(dbParams);
+db.connect();
+
+// Load the logger first so all (static) HTTP requests are logged to STDOUT
+// 'dev' = Concise output colored by response status for development use.
+//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+app.use(morgan("dev"));
+
+app.use(cors());
+app.use(express.json()); // => allows us to access the req.body
+
+// app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static("./client/build")); => for demonstration
+
+if (process.env.NODE_ENV === "production") {
+  //server static content
+  //npm run build
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
+console.log(__dirname);
+console.log(path.join(__dirname, "client/build"));
+
+
+
+
+// const usersRoutes = require("./routes/users");
+
+// app.use("/users", usersRoutes(db));
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
