ce111b133410d38c187b44ea2917a83487b21e4f
Kevin <kevin.rph.lee@gmail.com>
2022-05-04 23:34:13 -0700

    added heroku URL for CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
-    'http://localhost:3001'
+    'http://localhost:3001',
+    'pacific-springs-01105.herokuapp.com'
 ];
 
 module.exports = allowedOrigins;
