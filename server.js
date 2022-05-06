646d125fac1d46dc1ad6c57095b3cd239e4b91bf
Kevin <kevin.rph.lee@gmail.com>
2022-05-05 17:00:00 -0700

    heroku troubleshooting

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ app.use("/refresh", refreshRoutes());
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
