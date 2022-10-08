080fd0d2cddd396b6e362a18bb49b62dd6f960f4
Kevin <kevin.rph.lee@gmail.com>
2022-10-07 19:30:07 -0700

    updated index path

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.use('/orders', ordersRoutes());
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
   // res.sendFile(path.join(__dirname, '/client/build/index.html'));
-  res.sendFile(path.join(__dirname, '/client/public/index.html'));
+  res.sendFile(path.join(__dirname, '/app/client/public/index.html'));
 });
 
 
